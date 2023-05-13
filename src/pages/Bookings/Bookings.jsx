adb7a57cbdaa7ecc4645fac94d7222c3f8547714
AhasanulAlam <ahasanulalam@gmail.com>
2023-05-13 23:45:20 +0800

    carDoctor client update

---
+++ src/pages/Bookings/Bookings.jsx
@@ -0,0 +1,103 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
+
+const Bookings = () => {
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]);
+
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBookings(data))
+            .catch(error => {
+                console.error(error);
+            })
+    }, [url]);
+
+    const handleDelete = (id) => {
+        const proceed = confirm('Are you sure you want to delete!');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted Successful!!');
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining);
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        }
+    }
+
+    const handleBookingConfirm = (id) => {
+        const proceed = confirm('Are you sure you want to Confirm!');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ status: 'confirm' })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.modifiedCount > 0) {
+                        // Update state
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        const updated = bookings.find(booking => booking._id === id);
+                        updated.status = 'confirm';
+                        const newBooking = [updated, ...remaining];
+                        setBookings(newBooking);
+                    }
+                })
+        }
+    }
+
+    return (
+        <div>
+            <h2 className="text-5xl text-center">Your Bookings: {bookings.length}</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Service / Date</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            bookings.map(booking => <BookingRow
+                                key={booking._id}
+                                booking={booking}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
+                            ></BookingRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Bookings;
